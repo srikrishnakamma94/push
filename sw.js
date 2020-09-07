354f48e91da56393574dbd0e6e1c7ac5ca9c08c0
srikrishna kamma <srikrishnakamma@gmail.com>
2020-09-07 13:36:54 +0530

    test fun addded

--- sw.js
+++ sw.js
@@ -38,6 +38,8 @@ self.addEventListener('notificationclick', function (event) {
   // Android doesn't close the notification when you click on it
   // See: http://crbug.com/463146
   event.notification.close();
+  
+  test();
 
   // This looks to see if the current is already open and
   // focuses if it is
@@ -56,4 +58,11 @@ self.addEventListener('notificationclick', function (event) {
         }
       })
   );
+});
+
+self.addEventListener('notificationclose', function (e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+
+  console.log('Closed notification: ' + primaryKey);
 });
