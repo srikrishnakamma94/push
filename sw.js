b2aeff7f4659295790828354a6ebae106e6fc34a
srikrishna kamma <srikrishnakamma@gmail.com>
2020-09-07 15:43:54 +0530

    listner  added

--- sw.js
+++ sw.js
@@ -38,9 +38,6 @@ self.addEventListener('notificationclick', function (event) {
   // Android doesn't close the notification when you click on it
   // See: http://crbug.com/463146
   event.notification.close();
-  
-  // test function
-  self.test();
 
   // This looks to see if the current is already open and
   // focuses if it is
